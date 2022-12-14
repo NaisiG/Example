e8d4d30d30af4a8e48857dab6b57f09588eaa355
NaisiG <garkan320286@gmail.com>
2022-12-14 23:35:57 +0800

    Examples008_Dog

---
+++ Examples008_Dog/Program.cs
@@ -0,0 +1,29 @@
+﻿int count = 0;
+int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpead = 5;
+int friend = 1;
+
+while(distance > 10)
+{
+    if(friend == 1)
+    {
+        int time = distance / (secondFriendSpeed + dogSpead);
+        friend = 2;
+        distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+        count++;
+    }
+    if(friend == 2)
+    {
+        int time = distance / (firstFriendSpeed + dogSpead);
+        friend = 1;
+        distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+        count++;
+    }
+
+}
+
+Console.Write("Собака пробежит ");
+Console.Write(count);
+Console.Write(" раз");
